2be3ee9f10f076938010a79294a67219d226e6f2
Marcin Wielgos <kontakt.vabes@gmail.com>
2020-02-09 19:14:13 +0100

    Add search by city name or zipcode

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
-
-let searchCity = function() {
+let searchCity = function () {
   console.log("Szukam miast");
   let inputValue = document.getElementById("citySelection").value;
   console.log(inputValue);
@@ -9,7 +8,7 @@ let createCityHeading = function (li, headingText) {
   let divHeader = document.createElement("div");
   divHeader.classList.add("container");
   divHeader.classList.add("city");
-  li.classList.add(headingText); 
+  li.classList.add(headingText);
   let cityBasicInfo = document.createElement("div");
   cityBasicInfo.classList.add("cityBasicInfo");
   let h2 = document.createElement("h2");
@@ -43,14 +42,24 @@ let createCityLoader = function (li) {
 let getWeatherInfo = function (inputValue, li) {
 
   let getRequest = new XMLHttpRequest();
-  let apiQuery = "http://api.openweathermap.org/data/2.5/weather?zip="+ inputValue +",pl&appid=06b943a95cb96f43870fe4b241feb758&units=metric&lang=pl";
+  let regexZipcode = new RegExp("^\\d\\d-\\d\\d\\d$");
+  let apiQuery;
+  if (regexZipcode.test(inputValue)) {
+    console.log("zip");
+    apiQuery = "http://api.openweathermap.org/data/2.5/weather?zip=" + inputValue + ",pl&appid=06b943a95cb96f43870fe4b241feb758&units=metric&lang=pl";
+  } else {
+    console.log("cit");
+    apiQuery = "http://api.openweathermap.org/data/2.5/weather?q=" + inputValue + ",pl&appid=06b943a95cb96f43870fe4b241feb758&units=metric&lang=pl";
+  }
+  console.log(inputValue);
+
   getRequest.open('GET', apiQuery);
   getRequest.onload = function () {
     let receivedData = getRequest.responseText;
     setTimeout(function () {
       setUpWeatherInfo(receivedData, li);
     }, 1000);
-    
+
   }
   getRequest.send();
 
@@ -65,32 +74,32 @@ let setUpWeatherInfo = function (receivedData, li) {
   }
   console.log(receivedData);
   li.querySelector("h2").innerHTML = receivedData.name + ", " + receivedData.weather[0].description;
-  li.querySelector(".specificInfo").innerHTML = 
-  '<div class="specificInfoLeft">' +
+  li.querySelector(".specificInfo").innerHTML =
+    '<div class="specificInfoLeft">' +
     '<p>Ciśnienie <span>' + receivedData.main.pressure + ' hPa</span></p>' +
     '<p>Wilgotność <span>' + receivedData.main.humidity + '</span></p>' +
     '<p>Wiatr <span>' + receivedData.wind.speed + '</span><img src="/wind.png" alt="" class="windImg" style="transform: rotate(' + receivedData.wind.deg + 'deg"></p>' +
-  '</div>' +
-  '<div class="specificInfoRight">' +
+    '</div>' +
+    '<div class="specificInfoRight">' +
     '<p class="degrees">' +
-     receivedData.main.temp + '&ordm;C' +
+    receivedData.main.temp + '&ordm;C' +
     '</p>' +
     '<p class="degreesRealFeel">' +
-      'Odczuwalna: ' + receivedData.main.feels_like + '&ordm;C' +
+    'Odczuwalna: ' + receivedData.main.feels_like + '&ordm;C' +
     '</p>' +
-  '</div>';
+    '</div>';
 }
 
-let addCity = function() {
+let addCity = function () {
   console.log("Adding city...");
   let inputValue = document.getElementById("citySelection").value;
   let addedCities = document.getElementById("addedCities");
   let ul = document.getElementById("list");
   let li = document.createElement("li");
-  
+
   createCityHeading(li, inputValue);
   createCityLoader(li);
-  
+
   addedCities.appendChild(li);
   li.style.opacity = 1;
 
@@ -99,21 +108,61 @@ let addCity = function() {
 
 let setClock = function () {
   let clocks = document.querySelectorAll(".clock");
-  let now = new Date();
-  for (clock of clocks) {
-    clock.innerHTML = now.getHours() + ":" + now.getMinutes();
+
+  let getRequest = new XMLHttpRequest();
+  let apiQuery = "http://worldclockapi.com/api/json/utc/now";
+  let receivedData;
+  getRequest.open('GET', apiQuery);
+  getRequest.onload = function () {
+    receivedData = JSON.parse(getRequest.responseText);
+    let now = new Date(receivedData.currentDateTime);
+    for (clock of clocks) {
+      let clockMessage = now.getHours() + ":";
+      if (now.getMinutes() < 10) {
+        clockMessage += "0";
+      }
+      clockMessage += now.getMinutes();
+      clock.innerHTML = clockMessage;
+    }
+
+    let welcomeMessage = document.querySelector(".welcomeMessage");
+    if (now.getHours() >= 18 && now.getHours() < 21) {
+      welcomeMessage.innerHTML = "Dobry wieczór";
+
+    } else if (now.getHours() >= 21 || now.getHours() < 4) {
+      welcomeMessage.innerHTML = "Dobrej nocy";
+    } else {
+      welcomeMessage.innerHTML = "Dzień dobry";
+    }
+
+    let footerYearMsg;
+    if (now.getFullYear() > 2020) {
+      footerYearMsg = "2020 - " + now.getFullYear();
+    } else {
+      footerYearMsg = "2020";
+    }
+
+    document.querySelector(".footerYear").innerHTML = footerYearMsg;
   }
+  getRequest.send();
 
-  let welcomeMessage = document.querySelector(".welcomeMessage");
-  console.log(now.getHours());
-  if (now.getHours() >= 18 && now.getHours() < 21) {
-    welcomeMessage.innerHTML = "Dobry wieczór";
+}
 
-  } else if (now.getHours() >= 21 || now.getHours() < 4) {
-    welcomeMessage.innerHTML = "Dobrej nocy";
+let switchLoadingScreen = function (shouldEnable = " ") {
+  let loadingScreen = document.getElementById("loadingScreen");
+
+  if (shouldEnable === true) {
+    loadingScreen.style.display = "flex";
+    loadingScreen.style.opacity = 1;
+  } else if (shouldEnable === false) {
+    loadingScreen.style.opacity = 0;
+    setTimeout(function () {
+      loadingScreen.style.display = "none";
+    }, 175);
   } else {
-    welcomeMessage.innerHTML = "Dzień dobry";
+    window.getComputedStyle(loadingScreen).getPropertyValue("opacity") == 0 ? switchLoadingScreen(true) : switchLoadingScreen(false);
   }
+
 
 }
 
@@ -123,11 +172,18 @@ document.getElementById("citySelection").addEventListener('keypress', function (
   }
 });
 
-document.getElementById("citySelection").addEventListener("input", function() {
+document.getElementById("citySelection").addEventListener("input", function () {
   searchCity();
 })
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   console.log("Witaj w aplikacji!");
   setClock();
+  setTimeout(function () {
+    switchLoadingScreen();
+  }, 500);
+  window.setInterval(function () {
+    setClock();
+  }, 5000);
+
 })
